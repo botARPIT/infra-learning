d868a0f2384c86aeb315217a69bf1552cee43121
botARPIT <arpitchauhanofficial>
2026-04-13 16:36:36 +0530

    feat: add dequeue function

--- api/app/queues.py
+++ api/app/queues.py
@@ -2,5 +2,11 @@ import redis
 
 r = redis.Redis(host="localhost", port=6381, decode_responses=True)
 
+
 def enqueue_job(job_id: str):
     r.rpush("jobs", job_id)
+
+
+def dequeue_job():
+    item = r.blpop("jobs")
+    return item[1]
