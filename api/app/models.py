0e1e7a6c8f2d557457d6b37b9c7af06518928cc8
botARPIT <arpitchauhanofficial>
2026-04-21 13:02:27 +0530

    fix: add lambda triggers for updated_at and created_at columns

--- api/app/models.py
+++ api/app/models.py
@@ -1,26 +1,25 @@
-from sqlalchemy import Column, String, Integer, DateTime, Text
+from sqlalchemy import Column, String, Integer, DateTime, Text, Enum as SAEnum
 from datetime import datetime, timezone
+from enum import Enum
 from .db import Base
+
 
 
 class Job(Base):
     __tablename__ = "jobs"
 
-    id = Column(String, primary_key=True)
+    id            = Column(String, primary_key=True)
 
-    status = Column(String, nullable=False)
+    status        = Column(String, nullable=False)
 
-    result = Column(Text, nullable=True)
-    error = Column(Text, nullable=True)
+    result        = Column(Text, nullable=True)
+    error         = Column(Text, nullable=True)
 
-    retry_count = Column(Integer, default=0, nullable=False)
+    retry_count   = Column(Integer, default=0, nullable=False)
     lease_version = Column(Integer, default=0, nullable=False)
 
-    owned_by = Column(String, nullable=True)
+    owned_by      = Column(String, nullable=True)
 
-    created_at = Column(DateTime, default=datetime.now(timezone.utc), nullable=False)
-    updated_at = Column(DateTime, default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc), nullable=False)
-
-    claimed_at = Column(DateTime, nullable=True)
-    
-# TODO: Change update at to: updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow), use enum for states
+    created_at    = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), nullable=False)
+    updated_at    = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
+    claimed_at    = Column(DateTime(timezone=True), nullable=True)
