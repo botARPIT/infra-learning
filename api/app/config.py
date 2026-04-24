08a80df64b16ce16c53f9a8faf97347cb8e1e9a8
botARPIT <arpitchauhanofficial>
2026-04-24 21:58:29 +0530

    chore: add REDIS_HOST as env var

--- api/app/config.py
+++ api/app/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     DATABASE_URL: str
     REDIS_PORT: int = 6379
-
+    REDIS_HOST: str = "redis"
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8"
